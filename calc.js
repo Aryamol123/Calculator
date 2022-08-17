9c478180eac47b92a19a1b3d85573c97e779628d
Arya <aryamol1015@gmail.com>
2022-08-17 21:00:02 +0530

    Initial Commit

---
+++ calc.js
@@ -0,0 +1,12 @@
+let screen = document.getElementById("screen");
+function display(val){
+    document.getElementById("screen").value+=val;
+}
+function clearScreen(){
+    document.getElementById("screen").value=" ";
+}
+function equalClick(){
+    let text=document.getElementById("screen").value;
+    let result=eval(text);
+    document.getElementById("screen").value=result;
+}
